4f6ec0cff661eab85b7b0b9eabfd1b9995a3bd70
Emy69 <viejone83@gmail.com>
2023-09-21 13:46:08 -0700

    ya revisa parentesis

--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,3 +1,15 @@
-int calculate() {
-  return 6 * 7;
+bool estaBalanceada(String cadena) {
+  List<String> pila = [];
+  var listaCracteres = cadena.split('');
+  for (var caracter in listaCracteres) {
+    if (caracter == '(') {
+      pila.add(caracter);
+    } 
+    if (caracter == ')') {
+      if (pila.isEmpty) return false;
+      pila.removeLast();
+    }
+  }
+
+  return pila.isEmpty;
 }
--- test/dart_application_1_test.dart
+++ test/dart_application_1_test.dart
@@ -2,7 +2,29 @@ import 'package:dart_application_1/dart_application_1.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('si vacia esta balanceado', () {
+    var resultado = estaBalanceada('');
+    expect(resultado, equals (true));
   });
+
+test('(no esta balanceado)', () {
+    var resultado = estaBalanceada('(');
+    expect(resultado, equals (false));
+  });
+
+test(') no esta balanceado', () {
+    var resultado = estaBalanceada(')');
+    expect(resultado, equals (false));
+  });
+
+test('( d ) esta balanceado', () {
+    var resultado = estaBalanceada('()');
+    expect(resultado, equals (true));
+  });
+
+test(')( esta balanceado', () {
+    var resultado = estaBalanceada(')(');
+    expect(resultado, equals (false));
+  });
+
 }
